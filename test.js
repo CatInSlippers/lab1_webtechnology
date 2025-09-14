f35777e521f62f4da17d60ba7c644f67803625b6
CatInSlippers <slippersandcat@gmail.com>
2025-09-14 13:53:02 +0300

    Update and rename main.js to test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+import {triangle} from "./script.js";
+
+// const types = ['leg', 'hypotenuse', 'adjacent angle', 'opposite angle', 'angle'];
+
+//Тести на помилки
+// triangle('leg', 2, 'leg', 3);
+// triangle(2, 'leg', 4);
+// triangle(2, 'leb', 3, 'leg');
+// triangle(3.60, 'hypotenuse', 3.60, 'hypotenuse');
+// triangle(3.60, 'hypotenuse', 33.75, 'adjacent angle');
+// triangle(-3.60, 'hypotenuse', 33.75, 'angle');
+
+//Тести на правильний результат
+// triangle(3.60, 'hypotenuse', 33.75, 'angle');
+// triangle(2, 'leg', 3, 'leg');
+// triangle(3.60, 'hypotenuse', 2, 'leg');
+// triangle(3.60, 'hypotenuse', 3, 'leg');
+// triangle(2, 'leg', 33.75, 'opposite angle');
+// triangle(3, 'leg', 33.75, 'adjacent angle');
